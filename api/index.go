8f6cd7e78e4220918efbbe181b2a6da7ec5ba182
Amr Halim <amrhalim@seekasia.com>
2024-01-28 23:33:43 +0800

    Fix HTML

--- api/index.go
+++ api/index.go
@@ -9,7 +9,7 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	text := "Hi there, this is 3310 encoder program!"
-	fmt.Fprintf(w, "<h1>Welcome to 3310 Encoder!</h1>\n<p>English: %s\n3310 version: %s</p>",
+	fmt.Fprintf(w, "<h1>Welcome to 3310 Encoder!</h1><div><strong>English:</strong>%s</div><div><strong>3310 version:</strong>%s</div>",
 		text,
 		encoder.Encode(text),
 	)
